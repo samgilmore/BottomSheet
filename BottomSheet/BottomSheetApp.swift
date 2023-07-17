d70fca014b6869fc4826d79cd304e314844fa3a5
Sam Gilmore <sam.gilmore@willowtreeapps.com>
2023-07-17 11:26:31 -0400

    Initial Commit

---
+++ BottomSheet/BottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomSheetApp.swift
+//  BottomSheet
+//
+//  Created by Sam Gilmore on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct BottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
